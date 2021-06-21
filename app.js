faf3e1b9557207725493dd4969503c384e2d1e68
Mariana Mohr <marianamohr@gmail.com>
2021-06-21 19:07:15 -0300

    ajustado para certificados

--- app.js
+++ app.js
@@ -1,43 +1,32 @@
 const express = require('express');
 const app = express();
 
-app.use('/certificados',(req, res, next) => {
+app.use('/certificados', (req, res, next) => {
     res.status(200).send({
-        certificados: {
+        certificados: [
             {
-            "id": 1,
-            "username": "joaos", //obrigatório e único, permitindo caracteres `a-z` e `0-9` e máximo de caracteres deve ser 30
-            "name": "João da Silva", //obrigatório e máximo de caracteres deve ser 255
-            "description": "",
-            "groups": [15],
-            "expiration": 10, //representa o número de dias que o certificado é valido, o número deve estar entre 10 e 3650.
-            "expirated_at": "2020-10-21T13:45:11-03:00", //preenche com a data calculda com base no valor informado no campo expiration, não é cadastrado e nem editado
-            "created_at": "2020-10-21T13:45:11-03:00", //preenche com a data atual quando esta criando o certificado
-            "updated_at": "2020-10-21T13:45:11-03:00" //preenche com a data atual quando esta modificando o certificado
-          },
-          {
-            "id": 1,
-            "username": "joaos", //obrigatório e único, permitindo caracteres `a-z` e `0-9` e máximo de caracteres deve ser 30
-            "name": "João da Silva", //obrigatório e máximo de caracteres deve ser 255
-            "description": "",
-            "groups": [15],
-            "expiration": 10, //representa o número de dias que o certificado é valido, o número deve estar entre 10 e 3650.
-            "expirated_at": "2020-10-21T13:45:11-03:00", //preenche com a data calculda com base no valor informado no campo expiration, não é cadastrado e nem editado
-            "created_at": "2020-10-21T13:45:11-03:00", //preenche com a data atual quando esta criando o certificado
-            "updated_at": "2020-10-21T13:45:11-03:00" //preenche com a data atual quando esta modificando o certificado
-          },
-          {
-            "id": 1,
-            "username": "joaos", //obrigatório e único, permitindo caracteres `a-z` e `0-9` e máximo de caracteres deve ser 30
-            "name": "João da Silva", //obrigatório e máximo de caracteres deve ser 255
-            "description": "",
-            "groups": [15],
-            "expiration": 10, //representa o número de dias que o certificado é valido, o número deve estar entre 10 e 3650.
-            "expirated_at": "2020-10-21T13:45:11-03:00", //preenche com a data calculda com base no valor informado no campo expiration, não é cadastrado e nem editado
-            "created_at": "2020-10-21T13:45:11-03:00", //preenche com a data atual quando esta criando o certificado
-            "updated_at": "2020-10-21T13:45:11-03:00" //preenche com a data atual quando esta modificando o certificado
-          },
-        }
+                "id": 1,
+                "username": "joaos", //obrigatório e único, permitindo caracteres `a-z` e `0-9` e máximo de caracteres deve ser 30
+                "name": "João da Silva", //obrigatório e máximo de caracteres deve ser 255
+                "description": "",
+                "groups": [15],
+                "expiration": 10, //representa o número de dias que o certificado é valido, o número deve estar entre 10 e 3650.
+                "expirated_at": "2020-10-21T13:45:11-03:00", //preenche com a data calculda com base no valor informado no campo expiration, não é cadastrado e nem editado
+                "created_at": "2020-10-21T13:45:11-03:00", //preenche com a data atual quando esta criando o certificado
+                "updated_at": "2020-10-21T13:45:11-03:00" //preenche com a data atual quando esta modificando o certificado
+            },
+            {
+                "id": 2,
+                "username": "joaos", //obrigatório e único, permitindo caracteres `a-z` e `0-9` e máximo de caracteres deve ser 30
+                "name": "João da Silva", //obrigatório e máximo de caracteres deve ser 255
+                "description": "",
+                "groups": [15],
+                "expiration": 10, //representa o número de dias que o certificado é valido, o número deve estar entre 10 e 3650.
+                "expirated_at": "2020-10-21T13:45:11-03:00", //preenche com a data calculda com base no valor informado no campo expiration, não é cadastrado e nem editado
+                "created_at": "2020-10-21T13:45:11-03:00", //preenche com a data atual quando esta criando o certificado
+                "updated_at": "2020-10-21T13:45:11-03:00" //preenche com a data atual quando esta modificando o certificado
+            },
+        ]
     })
 });
 
