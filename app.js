9807f4356ba3e0e4568cbb84d80b882bb0e581a3
Mariana Mohr <marianamohr@gmail.com>
2021-06-22 11:06:20 -0300

    Resolvido erro de CORS e erro H10

--- app.js
+++ app.js
@@ -3,12 +3,43 @@ const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-const rotaCertificados = require('./routes/certificados');
+
+
+
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/certificados', rotaCertificados);
 app.use(bodyParser.json());
+
+app.use('/', (req, res, next) => {
+    res.status(200).send({
+        certificados: [
+            {
+                "id": 1,
+                "username": "joaos", //obrigatório e único, permitindo caracteres `a-z` e `0-9` e máximo de caracteres deve ser 30
+                "name": "João da Silva", //obrigatório e máximo de caracteres deve ser 255
+                "description": "",
+                "groups": [15],
+                "expiration": 10, //representa o número de dias que o certificado é valido, o número deve estar entre 10 e 3650.
+                "expirated_at": "2020-10-21T13:45:11-03:00", //preenche com a data calculda com base no valor informado no campo expiration, não é cadastrado e nem editado
+                "created_at": "2020-10-21T13:45:11-03:00", //preenche com a data atual quando esta criando o certificado
+                "updated_at": "2020-10-21T13:45:11-03:00" //preenche com a data atual quando esta modificando o certificado
+            },
+            {
+                "id": 2,
+                "username": "marias", //obrigatório e único, permitindo caracteres `a-z` e `0-9` e máximo de caracteres deve ser 30
+                "name": "Maria da Silva", //obrigatório e máximo de caracteres deve ser 255
+                "description": "",
+                "groups": [15],
+                "expiration": 10, //representa o número de dias que o certificado é valido, o número deve estar entre 10 e 3650.
+                "expirated_at": "2020-10-21T13:45:11-03:00", //preenche com a data calculda com base no valor informado no campo expiration, não é cadastrado e nem editado
+                "created_at": "2020-10-21T13:45:11-03:00", //preenche com a data atual quando esta criando o certificado
+                "updated_at": "2020-10-21T13:45:11-03:00" //preenche com a data atual quando esta modificando o certificado
+            },
+        ]
+    })
+});
+
 
 
 app.use((req, res, next) => {
