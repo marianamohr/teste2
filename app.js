a560e53ecde5453148d440e09e9252b96af1fbb6
Mariana Mohr <marianamohr@gmail.com>
2021-06-22 11:16:18 -0300

    Ajustando rotas

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
 
-
-
-
-
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.use('/', (req, res, next) => {
+app.use('/certificados', (req, res, next) => {
     res.status(200).send({
         certificados: [
             {
@@ -52,21 +42,6 @@ app.use((req, res, next) => {
         return res.status(200).send({});
     }
     next();
-});
-
-app.use((req, res, next) => {
-    const erro = new Error('Não encontrado');
-    erro.status = 404;
-    next(erro);
-});
-
-app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    return res.send({
-        erro: {
-            mensagem: error.mensage,
-        }
-    })
 });
 
 module.exports = app;
