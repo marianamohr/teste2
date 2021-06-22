4a53e80089f6a690cd33e78dff38e131a248c2aa
Mariana Mohr <marianamohr@gmail.com>
2021-06-22 14:03:46 -0300

    retirado if

--- app.js
+++ app.js
@@ -32,16 +32,16 @@ app.use('/certificados', (req, res, next) => {
 
 
 
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Header', 
-//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-//     );
-//     if(req.method === 'OPTIONS'){
-//         res.header('Access-Control-Allow-Methods','PUT', 'POST', 'PATCH', 'DELETE', 'GET');
-//         return res.status(200).send({});
-//     }
-//     next();
-// });
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Header', 
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    // if(req.method === 'OPTIONS'){
+    //     res.header('Access-Control-Allow-Methods','GET');
+    //     return res.status(200).send({});
+    // }
+    next();
+});
 
 module.exports = app;
